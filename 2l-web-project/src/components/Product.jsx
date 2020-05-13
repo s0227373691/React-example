3a5b33ef7e9940e837bae87390f34780a833a482
Elmer <s0227373691@gms.tku.edu.tw>
2020-05-13 17:21:47 +0800

    move Styled-components settings

--- 2l-web-project/src/components/Product.jsx
+++ 2l-web-project/src/components/Product.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Product = ({ name, url, price }) => {
+const Product = ({ id, name, url, price }) => {
+  // set Stlyed-components
+  const ProductContainer = styled.li`
+    width: 200px;
+    margin: 10px;
+    padding: 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+
+    &:hover {
+      background-color: rgb(230, 228, 228);
+      margin-top: 0px;
+      margin-bottom: 20px;
+      transition: 0.7s;
+    }
+
+    &:active {
+      margin: 10px;
+      transition: 0.1s;
+    }
+  `;
+
+  const ProductImg = styled.div`
+    width: 100%;
+    height: 200px;
+    background-image: url(${url});
+    background-size: cover;
+  `;
+
   return (
     <ProductContainer>
-      <ProductImg src={url} alt='Not Found' />
-      <p>{name}</p>
-      <section>${price}</section>
+      <a href="index.html" target="_blank">
+        <ProductImg />
+        <p>{name}</p>
+        <section>${price}</section>
+      </a>
     </ProductContainer>
   );
 };
 
 export default Product;
-
-
-// set Stlyed-components 
-const ProductContainer = styled.li`
-  width: 200px;
-  margin: 0 10px;
-`;
-
-const ProductImg = styled.img`
-  width: 100%;
-  height: 200px;
-  cursor: pointer;
-`;
