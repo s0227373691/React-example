827f13f0f7aec4070ddaa1efb27a3f6f9db6b8b1
Elmer <s0227373691@gms.tku.edu.tw>
2020-07-11 16:07:34 +0800

    change image url

--- KUSHINE/client/src/containers/ProductList.jsx
+++ KUSHINE/client/src/containers/ProductList.jsx
@@ -32,37 +32,40 @@ const products = [
   {
     id: 1,
     name: "潮男服飾】春季寬松慵懶風絲質",
-    url: require("../containers/imgClothes/11bbe986530242ac110004.jpg"),
+    url: require("../images/jpg/11bbe986530242ac110004.jpg"),
     price: 238,
   },
   {
     id: 2,
     name: "男裝| 無印良品MUJI",
-    url: require("./imgClothes/78bde1cbfe19abe234239f1e3a9c05be.jpg"),
+    url: require("../images/jpg/78bde1cbfe19abe234239f1e3a9c05be.jpg"),
     price: 305,
   },
   {
     id: 3,
     name: "英倫男裝流行搭配",
-    url: require("./imgClothes/3155375_L.jpg"),
+    url: require("../images/jpg/3155375_L.jpg"),
     price: 655,
   },
   {
     id: 4,
     name: "2011初秋潮男服饰搭配",
-    url: require("./imgClothes/20180322172021_259.jpg"),
+    url: require("../images/jpg/20180322172021_259.jpg"),
     price: 700,
   },
   {
     id: 5,
     name: "春夏流行服飾【創翊韓都】針織罩衫男裝寬鬆V領針織外套",
-    url: require("./imgClothes/3044393271363031.jpg"),
+    url: require("../images/jpg/3044393271363031.jpg"),
     price: 359,
   },
   {
     id: 6,
     name: "大尺碼 白襯衫 襯衫 男 加肥 大男服飾 小尺碼 寬鬆 白 襯衫",
-    url: require("./imgClothes/201109151717292557.jpg"),
+    url: require("../images/jpg/201109151717292557.jpg"),
     price: 670,
   },
 ];
+
+
+
