90ca08324a821867be4a8a3f1b7f906c3ead9755
Elmer <s0227373691@gms.tku.edu.tw>
2020-07-14 17:49:02 +0800

    Updates

---
+++ precache-manifest.ef3edc7cbab55cf30473d2cbf6231e44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c005a8f720788b04c3053379544ae983",
+    "url": "/React-example/index.html"
+  },
+  {
+    "revision": "99d0844bb707a7f549d2",
+    "url": "/React-example/static/js/2.9365f9fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-example/static/js/2.9365f9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fd37532d070a22b446",
+    "url": "/React-example/static/js/main.a25138ff.chunk.js"
+  },
+  {
+    "revision": "8044e7ce5931aa0bb491",
+    "url": "/React-example/static/js/runtime-main.b96a066a.js"
+  }
+]);
